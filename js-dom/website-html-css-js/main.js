d68cc268e57b9df7459650ad68800db410ae899e
kevinccbsg <kevinccbsg@gmail.com>
2022-05-26 16:54:59 +0200

    feat: basic rendering

--- js-dom/website-html-css-js/main.js
+++ js-dom/website-html-css-js/main.js
@@ -1,7 +1,58 @@
 
 const fromFile = 'Hello world!!!';
 // Selectors
+const items = document.querySelectorAll('.show-item');
+const showItems = document.getElementsByClassName('show-item');
+// document.getElementById
+
+// console.log([...items]);
+// console.log(showItems);
+// const showItemsArray = [...showItems];
+
+// items.forEach((item) => {
+//   console.log(item.innerHTML);
+// });
+
+// showItemsArray.forEach((item) => {
+//   console.log('showItemsArray item', item.innerHTML)
+// });
+
 const navbar = document.querySelector('.navbar');
 
-console.log(navbar);
+console.log(navbar.innerText);
+console.log(navbar.className);
+// navbar.innerText = 'Texto desde <b>javascript</b>';
+// navbar.innerHTML = 'Texto desde';
+// navbar.className = navbar.className + ' nueva js-example';
+navbar.classList.add('nueva');
+navbar.classList.add('js-example');
+
+setTimeout(() => {
+  navbar.classList.toggle('js-example');
+  navbar.classList.remove('nueva');
+}, 2000);
+
+setTimeout(() => {
+  navbar.classList.toggle('js-example');
+}, 5000);
+const animeList = document.getElementById('anime-list');
+
+const menuItems = [
+  'Detective conan',
+  'Full metal',
+  'Kill la kill',
+  'Shingeky no kiokin',
+];
+
+const createMenuItem = (name) => {
+  const li = document.createElement('li');
+  li.textContent = name;
+  return li;
+};
+menuItems.forEach((menuItem) => {
+  const liElement = createMenuItem(menuItem);
+  animeList.appendChild(liElement);
+});
+
+// heavy task
 // Eventos
